75761ce932d4ed2dff8df06a6cab18073aacf0bc
Rudhra-Rekapally <144738935+Rudhra-Rekapally@users.noreply.github.com>
2023-09-29 16:55:37 -0600

    Add files via upload

--- API_Assignment4/API_Assignment4/ContentView.swift
+++ API_Assignment4/API_Assignment4/ContentView.swift
@@ -1,5 +1,3 @@
-
-
 import SwiftUI
 
 struct Country: Codable, Identifiable {
@@ -8,17 +6,38 @@ struct Country: Codable, Identifiable {
     var capital: [String]?
     var flag: String
     var population: Int
-    
-
+    var translations: TranslationName
+    var car: CarName
+//    Didn't have time to resolve this so keeping them in comments
+//    var demonyms: DemonymsName
 }
-
 
 struct CountryName: Codable {
     var common: String
     var official: String
 }
 
-// This is the variable for country detailed view
+struct TranslationName: Codable {
+    var ara: Translation
+}
+
+struct Translation: Codable {
+    var common: String
+}
+
+struct CarName: Codable {
+    var side: String
+}
+
+//struct DemonymsName: Codable {
+//    var eng: Demonyms
+//}
+
+//struct Demonyms: Codable {
+//    var f: String
+//    var m: String
+//}
+
 struct CountryDetailView: View {
     var country: Country
     
@@ -30,42 +49,54 @@ struct CountryDetailView: View {
         Text("Country Common Name: \(country.name.common)")
             .padding()
         Text("Capital: \(country.capital?.first ?? "Unknown")")
-        
-        
+            .padding()
+        Text("Translations: \(country.translations.ara.common)")
+            .padding()
+        Text("Car-Side: \(country.car.side)")
+//        Text("Demonyms: \(country.demonyms.eng.f)")
     }
 }
 
 struct CountriesView: View {
-    @State var countries =  [Country]()
+    @State var countries = [Country]()
+    @State var searchText = ""
     
-    func getAllCountries() async -> () {
+    func getAllCountries() async {
         do {
             let url = URL(string: "https://restcountries.com/v3.1/all")!
             let (data, _) = try await URLSession.shared.data(from: url)
-            print(data)
             countries = try JSONDecoder().decode([Country].self, from: data)
         } catch {
             print("Error: \(error.localizedDescription)")
         }
     }
     
-    
     var body: some View {
         NavigationView {
-            List(countries) { country in
-                NavigationLink(destination: CountryDetailView(country: country)) { // use navigation link
-                    VStack(alignment: .leading) {
-                        Text("\(country.flag) • \(country.name.common)")
-                      
-                        
+            VStack {
+                TextField("Search for a country...", text: $searchText)
+                    .padding(7)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .padding(.horizontal, 10)
+                
+                List {
+                    ForEach(countries.filter {
+                        searchText.isEmpty || $0.name.common.lowercased().contains(searchText.lowercased())
+                    }) { country in
+                        NavigationLink(destination: CountryDetailView(country: country)) {
+                            VStack(alignment: .leading) {
+                                Text("\(country.flag) • \(country.name.common)")
+                            }
+                        }
                     }
                 }
+                .task {
+                    await getAllCountries()
+                }
             }
-            .task {
-                await getAllCountries()
-            }
+            .navigationTitle("Countries")
         }
-        .navigationTitle("Countries")
     }
 }
 
